0d6d0333cf018dd5bd489d59c370454b746b531a
KEEN <kyh0927@live.co.kr>
2018-11-20 23:50:28 +0900

    여기어때 모바일 React용

---
+++ src/Root.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/style.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Test
+      </div>
+    );
+  }
+}
+
+export default App;
