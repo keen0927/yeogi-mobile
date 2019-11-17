ea15f908869b4a93b21fafb85d30ce061e9e7f01
KEEN <kyh0927@live.co.kr>
2019-11-17 23:31:33 +0900

    191107

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Main } from './Main';
-export { default as List } from './List';
-export { default as Product } from './Product';
-export { default as Reservation } from './Reservation';
