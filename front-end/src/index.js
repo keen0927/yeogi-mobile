5b255985f60bfed4b14285ed45bf616e1696339d
KEEN <kyh0927@live.co.kr>
2019-11-21 23:59:28 +0900

    191121

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -8,12 +8,13 @@ import * as serviceWorker from './serviceWorker';
 import rootReducer, { rootSaga } from "./modules";
 import createSagaMiddleware from 'redux-saga';
 import { BrowserRouter } from "react-router-dom";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(sagaMiddleware)
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(rootSaga);
