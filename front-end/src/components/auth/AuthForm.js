c17a90f00fe6daaa604a58b32c97a69a94f69995
KEEN <kyh0927@live.co.kr>
2019-12-04 00:30:32 +0900

    191204

--- front-end/src/components/auth/AuthForm.js
+++ front-end/src/components/auth/AuthForm.js
@@ -32,24 +32,28 @@ const textMap = {
   register: '회원가입'
 };
 
-const AuthForm = ({ type }) => {
+const AuthForm = ({ type, form, onChange, onSubmit }) => {
 
   const text = textMap[type];
 
   return (
     <AuthFormBlock>
       <h3>{text}</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <StyledInput
           autoComplite="username"
           name="username"
-          placeholder="아이디 "
+          placeholder="아이디"
+          onChange={onChange}
+          value={form.username}
         />
         <StyledInput
           autoComplite="new-password"
           name="password"
           placeholder="비밀번호"
           type="password"
+          onChange={onChange}
+          value={form.password}
         />
         { type === 'register' && (
           <StyledInput
@@ -57,6 +61,8 @@ const AuthForm = ({ type }) => {
             name="passwordConfirm"
             placeholder="비밀번호 확인"
             type="password"
+            onChange={onChange}
+            value={form.passwordConfirm}
           />
         )}
         <ButtonWidthMarginTop fullWidth purple>
