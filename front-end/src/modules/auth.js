a1343f832d80586807ab9985bc193566cdb2c20b
KEEN <kyh0927@live.co.kr>
2019-12-06 01:41:33 +0900

    191206

--- front-end/src/modules/auth.js
+++ front-end/src/modules/auth.js
@@ -1,8 +1,22 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from "immer";
+import { createRequestActionTypes } from "../lib/createRequestSaga";
+import { takeLatest } from 'redux-saga/effects';
+import * as authAPI from '../lib/api/auth';
 
 const CHANGE_FILED = 'auth/CHANGE_FILED';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+// const REGISTER = 'auth/REGISTER';
+// const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+// const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+//
+// const LOGIN = 'auth/LOGIN';
+// const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+// const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
+
+const [ REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE ] = createRequestActionTypes('auth/REGISTER');
+const [ LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE ] = createRequestActionTypes('auth/LOGIN');
 
 export const changeField = createAction(
   CHANGE_FILED,
